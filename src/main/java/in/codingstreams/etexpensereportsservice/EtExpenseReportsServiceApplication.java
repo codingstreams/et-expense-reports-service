d1f692b1e97aea683ec2a69efcb917c4b25f8061
Coding Streams <akxhdev@outlook.com>
2024-02-04 22:24:49 +0530

    Initial Commit

---
+++ src/main/java/in/codingstreams/etexpensereportsservice/EtExpenseReportsServiceApplication.java
@@ -0,0 +1,13 @@
+package in.codingstreams.etexpensereportsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtExpenseReportsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtExpenseReportsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/codingstreams/etexpensereportsservice/EtExpenseReportsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.codingstreams.etexpensereportsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtExpenseReportsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
